3a6564ee2ecfe08f4ca0b9cc9955c975f7d55d6b
AzumattDev <azumattgaming@gmail.com>
2022-09-27 15:29:12 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ServerSync;
+using Steamworks;
+
+namespace MaxPlayerCount
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class MaxPlayerCountPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "MaxPlayerCount";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        private static readonly ManualLogSource MaxPlayerCountLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            ConfigSync.IsLocked = true;
+            _maxPlayers = config("1 - General", "MaxPlayerCount", 10,
+                "Override the player count that valheim checks for. Default is the vanilla max of 10.");
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                MaxPlayerCountLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                MaxPlayerCountLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                MaxPlayerCountLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        [HarmonyPatch(typeof(SteamGameServer), nameof(SteamGameServer.SetMaxPlayerCount))]
+        public static class ChangeSteamServerVariables
+        {
+            private static void Prefix(ref int cPlayersMax)
+            {
+                int maxPlayers = _maxPlayers.Value;
+                if (maxPlayers >= 1) cPlayersMax = maxPlayers;
+            }
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(ZNet), nameof(ZNet.RPC_PeerInfo))]
+        static IEnumerable<CodeInstruction> MaxPlayersPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            var found = false;
+            foreach (var instruction in instructions)
+            {
+                if (instruction.opcode == OpCodes.Ldc_I4_S)
+                {
+                    MaxPlayerCountLogger.LogDebug("Found Ldc_I4_S, changing the value to " + _maxPlayers.Value);
+                    yield return new CodeInstruction(OpCodes.Call, ReplacePlayerLimit);
+                    found = true;
+                }
+
+                yield return instruction;
+            }
+
+            if (found is false)
+                MaxPlayerCountLogger.LogError("Cannot find <Stdfld someField> in OriginalType.OriginalMethod");
+        }
+
+        private static int ReplacePlayerLimit()
+        {
+            return _maxPlayers.Value;
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<int> _maxPlayers = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", KeyboardShortcut.AllKeyCodes);
+        }
+
+        #endregion
+    }
+}
---
+++ Thunderstore/README.md
@@ -0,0 +1,25 @@
+> ## Installation Instructions
+***You must have BepInEx installed correctly! I can not stress this enough.***
+`Only required to be installed on the server.`
+
+1. Download the latest version of BepInEx from [here](https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/)
+2. Make sure it's installed correctly on the server.
+3. Put the DLL into your BepInEx\plugins folder.
+4. Launch the server. 
+5. Change the player limit found in the configuration file generated in `BepInEx/config` called `Azumatt.MaxPlayerCount.cfg` to the desired amount of players.
+6. You should now be able to go past the limit of 10 players or to the value you have specified.
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
